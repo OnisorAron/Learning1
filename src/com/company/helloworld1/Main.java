7924cf465f0da63542311ea58d0a63b6244bc139
Aron <onisor.aron@yahoo.com>
2018-11-09 19:51:12 +0200

    Random Number Generator Game :D

--- src/com/company/helloworld1/Main.java
+++ src/com/company/helloworld1/Main.java
@@ -1,6 +1,7 @@
 package com.company.helloworld1;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
@@ -9,16 +10,26 @@ public class Main {
         return scanner.nextInt();
     }
 
-    private static void printHelloWorld(int number) {
-        System.out.println("Hello World!" + number + " ");
-    }
-
     public static void main(String[] args) {
+        System.out.println("Jatekot fogunk jatszani! :)");
         System.out.println("Irj be egy szamot: ");
-        int n=readInt();
-       for ( int i=1 ; i<=n ; i++ ) {
-        printHelloWorld(i);
-
-       }
+        Random rand = new Random();
+        int n=rand.nextInt(100) + 1;
+        for ( int i=1 ; i<=5 ; i++ ) {
+            int x=readInt();
+            if (x<n) {
+                System.out.println("A generalt szam nagyobb.");
+            }
+            if (x>n) {
+                System.out.println("A generalt szam kisebb.");
+            }
+            if (x==n) {
+                System.out.println("Szerencsed volt es eltalaltad");
+            }
+            if (i==5) {
+                System.out.println("Nem talaltad el 5 probalkozasbol!");
+                System.out.println("Sajnalom!");
+            }
+        }
     }
 }
