88db732f741166bdac69215efc8e914fd3ebf085
Péter Pandula <peter@halcyonmobile.com>
2018-11-10 16:27:32 +0200

    [CR] Code review on the initial menu implementation.

--- src/com/company/learning/GuessingGame.java
+++ src/com/company/learning/GuessingGame.java
@@ -3,10 +3,13 @@ package com.company.learning;
 import java.util.Random;
 import java.util.Scanner;
 
+//TODO: None of these methods should be static! You will create an instance of this object.
 public class GuessingGame {
     private static final int LIFE_COUNT = 5;
     private static final int MAXIMUM_NUMBER = 100;
 
+    //TODO: Pay attention to Lint warnings. This should be package-private. Read about access modifiers in Java if
+    //TODO: you don't understand them.
     public static void startGame() {
 
     }
@@ -31,11 +34,15 @@ public class GuessingGame {
         System.out.println("Irj be egy szamot: ");
         final Random rand = new Random();
         int n = rand.nextInt(MAXIMUM_NUMBER) + 1;
+        //TODO: Always use English for naming variables, functions and classes.
         boolean jo = false;
+        //TODO: You should not make "jo" part of the for loop's condition, you should check it's value afterwards
+        //TODO: instead, to see which message to display to the user.
         for (int i = 0; i < LIFE_COUNT && !jo ; i++) {
             int x = readInt();
             if (x == n) {
                 jo = true;
+                //TODO: Why don't you break the loop here? And move displaying the message after the loop.
                 System.out.println(username + ", " + "Szerencsed volt es eltalaltad! Gratula");
             }
             if (i == 0 && x == n) {
@@ -47,6 +54,8 @@ public class GuessingGame {
             if (x > n) {
                 System.out.println("A generalt szam kisebb.");
             }
+            //TODO: Very bad idea to leave this condition within the for loop. Why don't you move it after it?
+            //TODO: The for loop should end here.
             if (i == 4) {
                 System.out.println(username + ", " + "nem talaltad el 5 probalkozasbol!");
                 System.out.println("Sajnalom!");
--- src/com/company/learning/Main.java
+++ src/com/company/learning/Main.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
+    //TODO: Why do you expect this method to return a String? It should not return anything, it should just start the
+    //TODO: guessing game. Also, consider giving it a better name as we might create other games later on.
+    //TODO: Why did you make this method public? It should never be accessed from outside this class, so it should be private.
+    //TODO: Lastly, you could simply write "(new GuessingGame()).startGame() in the main function - don't write methods
+    //TODO: that only have a single line."
     public static String startGame() {
         GuessingGame game = new GuessingGame();
         return game.startGame();
     }
 
+    //TODO: Method should be private, pay attention to Lint warnings (the yellow highlight).
     public static int readNumber() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 
+    //TODO: Properly display the options for the user so they can know how to use the menu.
     public static void main(String[] args) {
         int n = readNumber();
         if (n == 1) {
-
+            //TODO: I suppose you wanted to call the startGame() method here.
         }
     }
 
